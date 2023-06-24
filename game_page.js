456beea3ed4715b5485e11deb97039b181439156
yadu-sharma <93312818+yadu-sharma@users.noreply.github.com>
2023-06-24 15:09:38 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,8 @@
+
+player1_score = 0;
+player2_score = 0;
+document.getElementById("player1_name1").innerHTML = localStorage.getItem("player1_name");
+document.getElementById("player2_name1").innerHTML = localStorage.getItem("player2_name")
+document.getElementById("player1_score1").innerHTML = player1_score;
+document.getElementById("player2_score1").innerHTML = player2_score;
+
